b861d7b40099d1fa96b95acce6e56379e6f0f4a4
Lejla Begluk <lejla.begluk@outlook.com>
2021-08-08 17:35:26 +0200

    Generic services

---
+++ NewsPortal/NewsPortal/Controllers/BaseController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NewsPortal.WebAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsPortal.WebAPI.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController<T, TSearch> : ControllerBase
+    {
+        private readonly IService<T, TSearch> _service;
+
+        public BaseController(IService<T, TSearch> service)
+        {
+            _service = service;
+        }
+
+
+        [HttpGet]
+        //[AllowAnonymous]
+        public List<T> Get([FromQuery] TSearch search)
+        {
+            var list = _service.Get(search);
+            return (list);
+        }
+
+        [HttpGet("{id}")]
+        //[AllowAnonymous]
+        public T GetById(int id)
+        {
+            return _service.GetById(id);
+        }
+    }
+}
